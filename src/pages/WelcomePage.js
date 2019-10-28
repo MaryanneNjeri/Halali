833ecc90979f5fd5a98cb738c75b9a0214158025
maryanne <mary.jereh@gmail.com>
2019-10-28 16:25:34 +0300

    added row and column spaces

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -8,7 +8,13 @@ export default class WelcomePage extends React.Component {
       <MDBContainer fluid>
         <MDBRow>
           <MDBCol md="6" className="img-column">
-            <h3>Registration Process</h3>
+            <h1 className="landing-text"><b>Mastermind Better Succeed Together</b></h1>
+            <br/>
+            <p className="landing-text">
+Sed ut p      erspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+              veritatis et quasi architecto beatae vitae
+              {' '}
+            </p>
 
           </MDBCol>
           <MDBCol md="6">
