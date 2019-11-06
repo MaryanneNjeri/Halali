226d18ff4da6ca2df4eea681562dbb220c7aaf99
maryanne <mary.jereh@gmail.com>
2019-11-06 14:44:35 +0300

    added media queries and a welcome page to select the lawyer's role

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { MDBContainer } from "mdbreact";
-
+import { MDBContainer,MDBBtn } from "mdbreact";
+import welcome from '../assets/images/celebration.svg'
 export default class WelcomePage extends  React.Component{
     render() {
         return(
             <MDBContainer>
-                <p>Welcome</p>
+                <br/>
+                <br/>
+                <div>
+                    <h2 className="h2-responsive text-center welcome-text">Welcome to Halali</h2>
+                    <p className="text-center welcome-text">"consectetur adipiscing elit, sed do eiusmod tempor incididunt ut magna aliqua."</p>
+                  <img alt="welcome image" src={welcome} className="img-fluid welcome-image"/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                    <p className="text-center"><i>Welcome select your role to continue ....</i></p>
+                  <div className="role-buttons">
+                      <MDBBtn  gradient="peach" className="welcome-buttons">Partner</MDBBtn>
+                      <MDBBtn gradient="sunny-morning" className="welcome-buttons">Secretary</MDBBtn>
+                      <MDBBtn gradient="peach" className="welcome-buttons">Advocate</MDBBtn>
+                      <MDBBtn gradient="aqua" className="welcome-buttons">Intern</MDBBtn>
+                  </div>
+                </div>
             </MDBContainer>
         )
     }
