930a3776fb459f87d8fd04cb3ddb0b02d0332a8a
maryanne <mary.jereh@gmail.com>
2019-11-12 14:03:41 +0300

    added google maps to the registration page

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { MDBRow, MDBCol,MDBInput,MDBBtn } from 'mdbreact';
+import { MDBRow, MDBCol,MDBInput,MDBBtn,MDBCard,MDBCardBody } from 'mdbreact';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-export default class RegistrationPage extends React.Component {
+const mapStyles= {
+    width: '100%',
+    height:'25%',
+
+
+};
+class RegistrationPage extends React.Component {
    register=()=>{
         this.props.history.push('/welcome')
     };
@@ -12,29 +19,25 @@ export default class RegistrationPage extends React.Component {
           <MDBCol lg="8" md="8" sm="8" className="img-column"/>
           <MDBCol lg="4" md="4"  sm="4" className="form-column">
               <h3 className="h3-responsive text-center"><b>Registration</b></h3>
-              <div className="form">
-                  <MDBRow>
-                      <MDBCol md="6" sm="6">
-                          <MDBInput label="First Name" type="text" icon="user" iconClass="registration-icon" name="first_name" required />
-                      </MDBCol>
-                      <MDBCol md="6" sm="6">
-                          <MDBInput label="Last Name" type="text" icon="user" iconClass="registration-icon" name="last_name" required />
-                      </MDBCol>
-                  </MDBRow>
-                  <MDBInput label="Company Name" icon="pencil-alt"  iconSize="3x" iconClass="registration-icon" type="text" name="company_name" required />
-
-                  <MDBInput label="Email" type="email" icon="envelope" iconClass="registration-icon" name="email" required />
+              <MDBInput label="Company Name" icon="pencil-alt"  iconSize="3x" iconClass="registration-icon" type="text" name="company_name" required />
+              <MDBInput label="Email" type="email" icon="envelope" iconClass="registration-icon" name="email" required />
 
               <MDBInput label="Number of Employees" type="number"   iconClass="registration-icon" icon="users" name="no_of_employees" required />
 
               <MDBInput label="Address" type="text"  icon="map-marker" iconClass="registration-icon" name="company_address" required />
 
-              <MDBInput label="Role" type="text" icon="envelope" iconClass="registration-icon" name="Role" required />
-                   <div className="register-button-div">
+              <Map
+                  google={this.props.google}
+                  zoom={14}
+                  style={mapStyles}
+                  initialCenter={{lat:-1.28333 ,lng:36.8233}}
+              />
+              <div className="register-button-div">
                   <MDBBtn gradient="peach" onClick={this.register} className="register-button" >Register</MDBBtn>
-                       <div className="decoratedLine"><span><a href="/login">Already have an account?</a></span></div>
-                   </div>
+                  <br/>
+                       <div><span><a href="/login">Already have an account?</a></span></div>
               </div>
+
 
           </MDBCol>
         </MDBRow>
@@ -42,3 +45,7 @@ export default class RegistrationPage extends React.Component {
     );
   }
 }
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDYTORsIt6d7On403U205H4P42H5v6iI6U',
+})(RegistrationPage);
